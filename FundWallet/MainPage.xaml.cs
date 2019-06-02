3dd34d89d71d3fb93522a187faa82adf0d8cbdbf
Marcelo brito <marcelobritowd95@gmail.com>
2019-06-02 11:50:11 -0300

    Initial Commit, already created the main form

---
+++ FundWallet/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FundWallet
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lblMessage.Text = "Fund Wallet";
+        }
+
+
+        async void addFund(object sender, EventArgs e) {
+            await DisplayAlert("Success!", "Fund added with success", "OK");
+        }
+    }
+}
---
+++ FundWallet/Model/Fund.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FundWallet.Model
+{
+    public class Fund
+    {
+
+        private string Name { get; set; }
+        private string Quantity { get; set; }
+        private string UnitPrice { get; set; }
+        private DateTime PurchaseDate { get; set; }
+
+        public Fund(string name, string quantity, string price)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+            this.UnitPrice = price;
+            this.PurchaseDate = new DateTime();
+        }
+    }
+}
